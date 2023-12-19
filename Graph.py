83d8f5915115ab8d34ef24b84c65f79e24046ac9
Abdelrahman Ezzat <74915712+AbdelrahmanMohamed129@users.noreply.github.com>
2023-12-19 20:43:39 +0200

    Build index running

--- Graph.py
+++ Graph.py
@@ -1,15 +1,18 @@
 class Vertex(object):
     def __init__(self, key):
         self.key = key
-        self.neighbors = {}
+        self.neighbors = set()
         self.value=[None]
 
     def __init__(self, key,value):
         self.key = key
-        self.neighbors = {}
+        self.neighbors = set()
         self.value=value
-    def add_neighbor(self, neighbor, weight=0):
-        self.neighbors[neighbor] = weight
+
+    def add_neighbor(self, neighbor):
+        # print("self.neighbors", self.neighbors)
+        # print("neighbor", neighbor)
+        self.neighbors.add(neighbor)
 
     def __str__(self):
         return '{} neighbors: {} \n values: {}'.format(
@@ -18,11 +21,15 @@ class Vertex(object):
             ,self.value[:3]
         )
 
-    def get_connections(self):
-        return self.neighbors.keys()
+    # def get_connections(self):
+    #     return self.neighbors
 
-    def get_weight(self, neighbor):
-        return self.neighbors[neighbor]
+    # def get_weight(self, neighbor):
+    #     return self.neighbors[neighbor]
+    
+    # get neighbor vertices
+    def get_neighbors(self):
+        return self.neighbors
     
 class Graph(object):
     def __init__(self):
@@ -40,17 +47,20 @@ class Graph(object):
     def __contains__(self, key):
         return key in self.verticies
 
-    def add_edge(self, v1_info, v2_info, weight=0):
+    def add_edge(self, v1_info, v2_info):
         v1_key,v1_value=v1_info
         v2_key,v2_value=v2_info
         if v1_key not in self.verticies:
             self.add_vertex(Vertex(v1_key,v1_value))
         if v2_key not in self.verticies:
             self.add_vertex(Vertex(v2_key,v2_value))
-        self.verticies[v1_key].add_neighbor(self.verticies[v2_key], weight)
+        self.verticies[v1_key].add_neighbor(self.verticies[v2_key])
 
     def get_vertices(self):
         return self.verticies.keys()
 
     def __iter__(self):
         return iter(self.verticies.values())
+    
+    def __str__(self):
+        return 'Graph({})'.format(dict(self.verticies))
